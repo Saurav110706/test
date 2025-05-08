d10512a6f47aabf1f7ce10aa4db74bd883c5cc2e
Nayan <nayan.makanjee@gmail.com>
2025-05-08 12:12:54 +0200

    Yes i tried something

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -20,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //hello jou ma se poes, i need to test somethings.
     }
 }
